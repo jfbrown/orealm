86eb75d94d938048cb62645048d144ec33cdfca6
John (Jack) Brown <jackb@tune.com>
2015-04-16 22:48:13 -0700

    playing with graphs

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,16 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+require "neo4j/railtie"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Orealm
   class Application < Rails::Application
+    config.generators { |g| g.orm :neo4j }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
